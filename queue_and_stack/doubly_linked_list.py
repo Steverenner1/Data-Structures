6646aac514fd04f8d2f9cce5c7a90b755b964080
Steve Renner <che1111@hotmail.com>
2020-02-26 15:37:09 -0800

    Stretch, bug fixes

--- queue_and_stack/doubly_linked_list.py
+++ queue_and_stack/doubly_linked_list.py
@@ -69,7 +69,7 @@ class DoublyLinkedList:
             return None
         value = self.head.value
         self.delete(self.head)
-        return value.value
+        return value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
@@ -92,7 +92,7 @@ class DoublyLinkedList:
             return None
         value = self.tail.value
         self.delete(self.tail)
-        return value.value
+        return value
 
 
     """Removes the input node from its current spot in the 
